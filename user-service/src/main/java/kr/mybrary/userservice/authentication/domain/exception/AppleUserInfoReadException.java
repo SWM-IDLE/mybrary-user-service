bba50eb204b154baaf8bd67fb0eb6dfad07d184d
Seyoung Lee <0112leesy@naver.com>
2023-10-11 22:16:36 +0900

    [IDLE-241] feat: 애플 소셜로그인 시 예외 클래스 추가
      - AppleClientSecretNotCreatedException
      - ApplePrivateKeyReadException
      - AppleTokenParseException
      - AppleUserInfoReadException
      - AppleUserNotFoundException

---
+++ user-service/src/main/java/kr/mybrary/userservice/authentication/domain/exception/AppleUserInfoReadException.java
@@ -0,0 +1,15 @@
+package kr.mybrary.userservice.authentication.domain.exception;
+
+import kr.mybrary.userservice.global.exception.ApplicationException;
+
+public class AppleUserInfoReadException extends ApplicationException {
+
+    private static final int STATUS = 404;
+    private static final String ERROR_CODE = "A-10";
+    private static final String ERROR_MESSAGE = "Apple 사용자 정보를 읽어오는데 실패했습니다.";
+
+    public AppleUserInfoReadException() {
+        super(STATUS, ERROR_CODE, ERROR_MESSAGE);
+    }
+
+}
