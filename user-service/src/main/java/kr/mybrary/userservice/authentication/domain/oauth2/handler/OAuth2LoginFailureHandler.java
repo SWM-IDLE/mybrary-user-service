898fb68b4d7a2508cd1c2d82b3a3843251feb99d
Seyoung Lee <0112leesy@naver.com>
2023-10-22 22:29:41 +0900

    [IDLE-261] feat: OAuth2LoginFailureHandler 수정
      - 기존: 응답 body에서 errorCode, errorMessage 전달
      - 문제: 웹뷰에서 리턴되는 응답 body를 모바일에서 읽어올 수 없음
      - 수정: redirect url로 errorCode, errorMessage 응답

--- user-service/src/main/java/kr/mybrary/userservice/authentication/domain/oauth2/handler/OAuth2LoginFailureHandler.java
+++ user-service/src/main/java/kr/mybrary/userservice/authentication/domain/oauth2/handler/OAuth2LoginFailureHandler.java
@@ -2,46 +2,38 @@ package kr.mybrary.userservice.authentication.domain.oauth2.handler;
 
 import static kr.mybrary.userservice.authentication.domain.oauth2.OAuth2Exception.SOCIAL_LOGIN_FAILED;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 @Slf4j
 public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
 
-    static final String ENCODING_UTF_8 = "UTF-8";
-    static final String CONTENT_TYPE_JSON = "application/json";
-
-    @Autowired
-    private ObjectMapper objectMapper;
+    static final String CALLBACK_URL = "kr.mybrary://";
+    static final String ERROR_CODE = "errorCode";
+    static final String ERROR_MESSAGE = "errorMessage";
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException exception) throws IOException {
         log.info("OAuth2 Login Failure Handler 실행 - OAuth2 로그인 실패 : " + exception.getMessage());
-        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        response.setCharacterEncoding(ENCODING_UTF_8);
-        response.setContentType(CONTENT_TYPE_JSON);
-        response.getWriter().write(generateResponseBody(exception));
-    }
 
-    private String generateResponseBody(AuthenticationException exception)
-            throws JsonProcessingException {
-        Map<String, String> responseMessage = new HashMap<>();
-        responseMessage.put("errorCode", getErrorCode(exception));
-        responseMessage.put("errorMessage", getErrorMessage(exception));
-        return objectMapper.writeValueAsString(responseMessage);
+        String url = UriComponentsBuilder.fromUriString(CALLBACK_URL)
+                .queryParam(ERROR_CODE, getErrorCode(exception))
+                .queryParam(ERROR_MESSAGE, getErrorMessage(exception))
+                .toUriString();
+
+        response.sendRedirect(url);
+        log.info("Redirect Url: " + url);
     }
 
     private String getErrorCode(AuthenticationException exception) {
