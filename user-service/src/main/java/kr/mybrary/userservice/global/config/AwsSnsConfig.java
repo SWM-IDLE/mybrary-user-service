58192cee9a5ce8ad0dcb6a2715e391216ad326c0
minseong <kdsvip5@naver.com>
2023-11-22 16:50:30 +0900

    [IDLE-314] config: AWS SnS Config 설정

---
+++ user-service/src/main/java/kr/mybrary/userservice/global/config/AwsSnsConfig.java
@@ -0,0 +1,38 @@
+package kr.mybrary.userservice.global.config;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sns.SnsClient;
+
+@Configuration
+@Slf4j
+@Getter
+public class AwsSnsConfig {
+
+    @Value("${cloud.aws.sns,credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.sns,credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.sns,region.static}")
+    private String region;
+
+    @Value("${cloud.aws.sns.topic.arn}")
+    private String topicArn;
+
+    @Bean
+    public SnsClient getSnsClient() {
+        return SnsClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)
+                )).build();
+    }
+}
