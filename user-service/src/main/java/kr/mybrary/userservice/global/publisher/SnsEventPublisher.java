9df6a3c7827847cdb93b46ad514801384ca893d1
Seyoung Lee <0112leesy@naver.com>
2023-11-23 21:13:36 +0900

    [hotfix] publishToSns 메서드 수정

--- user-service/src/main/java/kr/mybrary/userservice/global/publisher/SnsEventPublisher.java
+++ user-service/src/main/java/kr/mybrary/userservice/global/publisher/SnsEventPublisher.java
@@ -5,11 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Map;
 import kr.mybrary.userservice.global.config.AwsSnsConfig;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.PublishRequest;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class SnsEventPublisher {
 
@@ -19,18 +21,22 @@ public class SnsEventPublisher {
 
     public void publishToSns(Map<String, Object> messageData, SnsTopic topic) {
 
+        String message;
         try {
-            PublishRequest publishRequest = PublishRequest.builder()
-                    .topicArn(awsConfig.getTopicArn() + topic.getTopicName())
-                    .subject(MESSAGE_SUBJECT)
-                    .message(objectMapper.writeValueAsString(messageData))
-                    .build();
-
-            SnsClient snsClient = awsConfig.getSnsClient();
-            snsClient.publish(publishRequest);
-
+            message = objectMapper.writeValueAsString(messageData);
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Failed to convert message data to JSON", e);
         }
+
+        String topicArn = awsConfig.getTopicArn() + topic.getTopicName();
+        PublishRequest publishRequest = PublishRequest.builder()
+                .topicArn(topicArn)
+                .subject(MESSAGE_SUBJECT)
+                .message(message)
+                .build();
+
+        SnsClient snsClient = awsConfig.getSnsClient();
+        snsClient.publish(publishRequest);
+        log.info("Publishing message to SNS MESSAGE: {}", message);
     }
 }
