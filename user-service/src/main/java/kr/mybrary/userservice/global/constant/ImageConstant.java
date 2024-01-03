8b0b8f01d4107586cef6ea0c137258b35466bc46
Seyoung Lee <0112leesy@naver.com>
2024-01-03 22:03:27 +0900

    [hotfix] ImageConstant DEFAULT_PROFILE_IMAGE 수정

--- user-service/src/main/java/kr/mybrary/userservice/global/constant/ImageConstant.java
+++ user-service/src/main/java/kr/mybrary/userservice/global/constant/ImageConstant.java
@@ -2,9 +2,9 @@ package kr.mybrary.userservice.global.constant;
 
 public enum ImageConstant {
 
-    DEFAULT_PROFILE_IMAGE("https://mybrary-user-service.s3.ap-northeast-2.amazonaws.com/profile/profileImage/default.jpg"),
-    DEFAULT_PROFILE_IMAGE_TINY("https://mybrary-user-service-resized.s3.ap-northeast-2.amazonaws.com/tiny-profile/profileImage/default.jpg"),
-    DEFAULT_PROFILE_IMAGE_SMALL("https://mybrary-user-service-resized.s3.ap-northeast-2.amazonaws.com/small-profile/profileImage/default.jpg");
+    DEFAULT_PROFILE_IMAGE("https://mybrary-user.s3.ap-northeast-2.amazonaws.com/profile/profileImage/default.jpg"),
+    DEFAULT_PROFILE_IMAGE_TINY("https://mybrary-user-resized.s3.ap-northeast-2.amazonaws.com/tiny-profile/profileImage/default.jpg"),
+    DEFAULT_PROFILE_IMAGE_SMALL("https://mybrary-user-resized.s3.ap-northeast-2.amazonaws.com/small-profile/profileImage/default.jpg");
 
     private final String url;
 
